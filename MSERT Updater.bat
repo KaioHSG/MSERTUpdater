5d86e87204a7d6d5c91c3bf28dea2c24bcd828a6
Kaio HSG <96930584+KaioHSG@users.noreply.github.com>
2024-05-01 13:08:27 -0300

    Update MSERT Updater.bat

--- MSERT Updater.bat
+++ MSERT Updater.bat
@@ -1,8 +1,10 @@
 @echo off
+
 set MsertFolder=C:\Temp\Microsoft Safety Scanner
 set MsertServerTest=go.microsoft.com
 set Msert64Server=https://go.microsoft.com/fwlink/?LinkId=212732
 set Msert86Server=https://go.microsoft.com/fwlink/?LinkId=212733
+
 title Microsoft Safety Scanner Updater
 tasklist /nh /fi "imagename eq MSERT.exe" | findstr /l /i "MSERT.exe" > nul
 if %ErrorLevel% equ 0 (
@@ -10,7 +12,7 @@ if %ErrorLevel% equ 0 (
    timeout /t 5 > nul
    exit
 )
-wmic os get osarchitecture | findstr /l /c:"64"
+wmic os get osarchitecture | findstr /l /c:"64" > nul
 if %ErrorLevel% equ 0 (
    set MsertServer=%Msert64Server%
 ) else (
